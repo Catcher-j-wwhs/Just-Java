f838240d90ef8e9d8d52ed45af0b3480013cd090
catcher jones <catchernemail@gmail.com>
2022-09-06 12:31:02 -0500

    error code

--- app/src/main/java/com/example/justjava/MainActivity.kt
+++ app/src/main/java/com/example/justjava/MainActivity.kt
@@ -1,11 +1,42 @@
-package com.example.justjava
+package com.example.android.justjava;
+/**
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
+IMPORTANT: Make sure you are using the correct package name.
+This example uses the package name:
+package com.example.android.justjava
+If you get an error when copying this code into Android studio, update it to match teh package name found
+in the project's AndroidManifest.xml file.
+ **/
+import android.widget.TextView;
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+/**
+
+This app displays an order form to order coffee.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @OverRide
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+
+    This method is called when the order button is clicked.
+     */
+    public void submitOrder(View view) {
+        display(1);
+    }
+    /**
+
+    This method displays the given quantity value on the screen.
+     */
+    private void display(int number) {
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
     }
 }
